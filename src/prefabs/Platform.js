39a49cd9ad50ecafa2c7c291f9181dd548a390b7
Bowen Zhao <bzhao19@ucsc.edu>
2020-04-30 17:06:03 -0400

    platform falling finished

--- src/prefabs/Platform.js
+++ src/prefabs/Platform.js
@@ -12,7 +12,9 @@ class Platform extends Phaser.Physics.Arcade.Sprite{
         //initiate velocity : maybe implement random x movement later version
         this.body.velocity.x = 0;
         //velocity used for platform falling
-        this.body.velocity.y += -1;
+        this.body.velocity.y = 0;
+        //seperate gravity calculation for platform falling, can be adjust later for increasing speed over time
+        this.body.gravity.y = 5000;
        
         if(this.y > game.config.height){
             this.reset();

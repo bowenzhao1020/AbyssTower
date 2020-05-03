a8ac0814461cbbc685fecbb8f9cf4359546d16df
Elvis Ng <eng18@ucsc.edu>
2020-05-03 03:12:13 -0700

    update

--- src/scenes/Instru.js
+++ src/scenes/Instru.js
@@ -40,17 +40,7 @@ class Instru extends Phaser.Scene{
         this.add.text(centerX, 150, 'Press ← and → to move around', instruConfig).setOrigin(0.5);
         this.add.text(centerX, 200, 'Press space bar to jump', instruConfig).setOrigin(0.5);
 
-        this.add.text(centerX, 275, 'But Remember!', instruConfig).setOrigin(0.5);
-        this.add.text(centerX, 325, 'Avoid the monster!', instruConfig).setOrigin(0.5);
-        this.add.text(centerX, 350, '↓', instruConfig).setOrigin(0.5);
-        this.add.tileSprite(centerX, 400, 50, 50, 'monster').setOrigin(0.5);
-
-        this.add.text(centerX, 475, 'Use these to slow down', instruConfig).setOrigin(0.5);
-        this.add.text(centerX, 525, 'the endless monster wave', instruConfig).setOrigin(0.5);
-        this.add.text(centerX, 550, '↓', instruConfig).setOrigin(0.5);
-        this.add.tileSprite(centerX, 600, 50, 50, 'watch').setOrigin(0.5);
-
-        this.add.text(centerX, 700, 'Press Space to Escape from HELLLLL', instruConfig).setOrigin(0.5);
+        this.add.text(centerX, 300, 'Press Space to Escape from HELLLLL', instruConfig).setOrigin(0.5);
 
         //space key scene change
         this.key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
